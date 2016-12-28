496e8f87d832a517eabe2233b8d3899281f502af
Azoghal <sam.beer208@gmail.com>
2016-12-28 15:19:12 +0000

    doing shields

--- shield.lua
+++ shield.lua
@@ -1,1 +1,11 @@
 shield = class('shield')
+
+function shield:initalize()
+	self.radius = 200
+	self.colour = {0,255,255}
+	self.dAngle = 0
+	self.pointA = {}
+	self.pointB = {}
+	self.strength = 100
+	self.AngleDif = 90
+end
