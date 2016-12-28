0241d3aa61b90af2009ca6ba760d64b6788e4279
Azoghal <sam.beer208@gmail.com>
2016-12-28 15:28:34 +0000

    tidies up the stars

--- main.lua
+++ main.lua
@@ -8,23 +8,50 @@ require 'shield'
 function love.load()
 	ship = shipA:new()
 	stars = {}
+
+	
+
 	love.window.setMode( 600, 800)
 
-	for i=0,200,1 do
-		star = {}
-		star.x, star.y = math.random(0,800), math.random(0,800)
-		table.insert(stars,star)
+	function starMake()
+
+		for i=0,200,1 do
+			star = {}
+			star.x, star.y = math.random(0,600), math.random(0,800)
+		end
+
+		for i=0,200,1 do
+			star = {}
+			star.x, star.y = math.random(0,800), math.random(0,800)
+
+			table.insert(stars,star)
+		end
+
 	end
+
+	function StarUpd() 
+		for i,e in ipairs(stars) do
+			e.y = e.y + 1
+			if e.y > 800 then
+				e.y, e.x = 0, math.random(800)
+			end
+		end
+	end
+
+	starMake()
 
 end
 
 function love.update()
-	for i,e in ipairs(stars) do
-		e.y = e.y + 1
-	end
+	StarUpd()
 end
 
 function love.draw()
+
+	for i,e in ipairs(stars) do
+		love.graphics.circle("fill",e.x,e.y,2)
+	end
+
 
 	for i,e in ipairs(stars) do
 		love.graphics.circle("fill",e.x,e.y,2)
