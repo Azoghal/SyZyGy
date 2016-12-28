be51861d59b24c58952810834a8bf040bb96c890
Azoghal <sam.beer208@gmail.com>
2016-12-28 12:42:25 +0000

    changed stiff

--- main.lua
+++ main.lua
@@ -7,12 +7,30 @@ require 'shield'
 
 function love.load()
 	ship = shipA:new()
+	stars = {}
+	love.window.setMode( 600, 800)
+
+	for i=0,200,1 do
+		star = {}
+		star.x, star.y = math.random(0,800), math.random(0,800)
+		table.insert(stars,star)
+	end
+
 end
 
 function love.update()
+	for i,e in ipairs(stars) do
+		e.y = e.y + 1
+	end
 end
 
 function love.draw()
+
+	for i,e in ipairs(stars) do
+		love.graphics.circle("fill",e.x,e.y,2)
+	end
+
 	love.graphics.draw(ship.image,ship.x,ship.y,0,1,1,50,75)
+
 end
 
